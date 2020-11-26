177fa35dc4110e21d406f3796c3f1efbb74aeeed
huccgreen <huccgreen@gmail.com>
2020-11-26 18:12:00 +0200

    fixed task 9 and 5 as per review

--- task5.js
+++ task5.js
@@ -8,6 +8,19 @@ The function should return the area of a triangle.
 
 
 
-function task5(side1, side2, side3) {
+function semiPerimeter(side1, side2, side3) {
     return (side1 + side2 + side3) / 2
 }
+
+
+function task5(side1, side2, side3) {
+    var semiP = semiPerimeter(side1, side2, side3)
+
+    return Math.sqrt((semiP) * (semiP - side1) * (semiP - side2) * (semiP - side3))
+}
+
+
+
+
+console.log(task5(5, 6, 5));
+
