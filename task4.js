1e8f9bee40c6d090c1d9a3d36b10371a019d408c
huccgreen <huccgreen@gmail.com>
2020-11-26 17:53:31 +0200

    minor useless fixes

--- task4.js
+++ task4.js
@@ -1,28 +1,12 @@
 /*
-
-Write a function that takes 2 numbers as input.
-If either of the numbers is 3 AND the sum of the numbers
-contains a 3 then return true. Otherwise return false
-
+Write a function that takes in three numbers.
+These numbers represent the lengths of the sides of a triangle.
+The function should return the area of a triangle.
 */
 
 
 
-//This is a function that will check if the sum of 2 numbers contains a 3
-
-function sumContains3(n1, n2) {
-    var sum = n1 + n2;
-    if (sum.toString().includes(3)) {    //convert the number to a string first so that you can use the string operation .includes() on it
-        return true
-    }
-    else {
-        return false
-    }
-
+function task5(side1, side2, side3) {
+    return (side1 + side2 + side3) / 2
 }
 
-
-function task4(num1, num2) {
-    if ((num1 == 3 || num2 == 3) && sumContains3(num1, num2)) { return true }
-    else { return false }
-}
