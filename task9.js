7bd890ef5dd4e36adb9d4abdf0bae620d9e861cf
huccgreen <huccgreen@gmail.com>
2020-11-27 13:11:41 +0200

    moved the sum variable declaration inside the function

--- task9.js
+++ task9.js
@@ -1,8 +1,9 @@
 /* Find the sum of all the multiples of 3 or 5 below 1000.*/
 
-sum = 0;
 
 function task9() {
+    var sum = 0;
+
     for (var i = 0; i < 1000; i++) {
 
         if (i % 3 == 0 || i % 5 == 0) {
